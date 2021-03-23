fc2daa0763d2b57108b2c8236dec6f726189ff8c
Pratik Shahi <shahipr0@gmail.com>
2021-03-23 19:45:11 +0530

    attached all the EJS pages to app.js

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 //jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
+
+app.route("/").get(function (req, res) {
+  res.render("home");
+});
+
+app.route("/login").get(function (req, res) {
+  res.render("login");
+});
+
+app.route("/register").get(function (req, res) {
+  res.render("register");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
