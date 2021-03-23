0aafcda4b51b54fec40c3adfa1c27b9d3a398512
Pratik Shahi <shahipr0@gmail.com>
2021-03-23 21:56:05 +0530

    encrypted password using mongoose encryyption

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -20,10 +22,13 @@ mongoose.connect("mongodb://localhost:27017/authWebUserDB", {
   useUnifiedTopology: true,
 });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
+//for password encryption
+const secret = "ThisIsOurSecret"; //secret to encrypt password
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
